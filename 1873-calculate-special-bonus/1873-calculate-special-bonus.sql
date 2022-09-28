d7806046553bc22058675ca512383b08ee3a4e4f
토즐라 <innuendobeat@gmail.com>
2022-09-28 12:36:46 +0900

    Time: 591 ms (81.22%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,3 +1,3 @@
-SELECT employee_id, IF(employee_id %2 = 1 AND LEFT(name, 1) != 'M', salary, 0) as bonus
+SELECT employee_id, IF ((employee_id % 2 = 1) AND (LEFT(name, 1) != 'M'), salary, 0) as bonus
 FROM Employees
 ORDER BY employee_id;
